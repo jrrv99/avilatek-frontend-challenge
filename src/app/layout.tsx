f062adab4b6356f7bd79c9f0a0f729fd0f4631cf
José Riobueno <jrrv99@gmail.com>
2025-07-11 11:46:07 -0400

    feat: add Header component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+
+import Header from '@/components/Header';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,6 +27,7 @@ const RootLayout = ({
 }>) => (
   <html lang="en">
     <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <Header />
       {children}
     </body>
   </html>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from '@/components/Logo';
+
+const Header = () => {
+  return (
+    <header className="px-4 py-2">
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
