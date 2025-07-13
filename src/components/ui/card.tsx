a9169abf572947e7b4fc7b3f90191524d702f1da
José Riobueno <jrrv99@gmail.com>
2025-07-12 21:29:29 -0400

    fix(ui): update CardTitle component to apply primary text color

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -27,7 +27,7 @@ const CardHeader = ({ className, ...props }: ComponentProps<'div'>) => (
 const CardTitle = ({ className, ...props }: ComponentProps<'div'>) => (
   <div
     data-slot="card-title"
-    className={cn('leading-none font-semibold', className)}
+    className={cn('leading-none font-semibold text-primary', className)}
     {...props}
   />
 );
